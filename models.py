8b7bc175100174ca7a7d597a27c7afa6a4033740
Areena Mahek <63315474+AreenaMahek@users.noreply.github.com>
2025-02-25 21:23:07 -0600

    Update models.py

--- models.py
+++ models.py
@@ -2,29 +2,20 @@ import os
 import uuid
 from django.db import models
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 
 
-
-# function to return the relative path inside the datastore directory
-'''def get_datastore_path(instance, filename):
-    user_ds_path = os.path.join(str(instance.user_id), str(instance.datastore_id))
-    user_ds_bucket_path = os.path.join(user_ds_path, str(instance.bucket_id))
-    return os.path.join(user_ds_bucket_path, str(filename))'''
-
+# Create your models here.
 
 
 class UserDatastore(models.Model):
     """ Model to store a persistent datastore_id for each user created on an instance of BB """
 
-    username = models.CharField(max_length=100, blank=True)
-    password = models.CharField(max_length=100, blank=True)
-    user_id = models.IntegerField(blank=True, null=True) # ForeignKey to the User model
+    user_id = models.IntegerField(blank=True, null=True) 
+    instance_id = models.UUIDField(default=uuid.uuid4, editable=False) 
     datastore_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     datastore_name = models.CharField(max_length=100, blank=True)
-    ds_default = models.BooleanField(default=True)
     datastore_private = models.BooleanField(default=True) # True if the datastore is private to the user, False if it is public
-    instance_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False) 
+    ds_default = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
 
@@ -36,18 +27,17 @@ class UserDatastore(models.Model):
 class UserBucket(models.Model):
     """ Model to store a persistent datastore_id for each user created on an instance of BB """
 
-    username = models.CharField(max_length=100, blank=True)
-    password = models.CharField(max_length=100, blank=True)
+    
     user_id = models.IntegerField(blank=True, null=True) 
+    instance_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    
     datastore_id = models.ForeignKey(UserDatastore, on_delete=models.CASCADE) # ForeignKey to the UserDatastore model
-    datastore_name = models.CharField(max_length=100, blank=True)
-    datastore_private = models.BooleanField(default=True) # True if the datastore is private to the user, False if it is public
+
     bucket_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     bucket_name = models.CharField(max_length=100, blank=True)
-    ds_default = models.BooleanField(default=True)
-    bucket_default = models.BooleanField(default=True)
     bucket_private = models.BooleanField(default=True) # True if the bucket is private to the user, False if it is public
-    instance_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    bucket_default = models.BooleanField(default=True)
+    
     created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
@@ -57,14 +47,18 @@ class UserBucket(models.Model):
 class UploadedFileModel(models.Model):
     """ Model to store a persistent file upload path with datastore id and user id for each user with privacy settings """
     
+    file_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     user_id = models.IntegerField(blank=True, null=True)
-    datastore_id = models.CharField(max_length=200, blank=True) # ForeignKey to the UserDatastore model
-    bucket_id = models.CharField(max_length=200, blank=True) # ForeignKey to the UserBucket model
+    instance_id = models.UUIDField(default=uuid.uuid4, editable=False)
+    
+    datastore_id = models.ForeignKey(UserDatastore, on_delete=models.CASCADE) # ForeignKey to the UserDatastore model
+    bucket_id = models.ForeignKey(UserBucket, on_delete=models.CASCADE) # ForeignKey to the UserBucket model
+
     file_name = models.CharField(max_length=100, blank=True)
-    file_size = models.IntegerField(blank=True, null=True) # size of the file in bytes
     file_type = models.CharField(max_length=100, blank=True)
+    file_size = models.IntegerField(blank=True, null=True) # size of the file in bytes
     file_private = models.BooleanField(default=True) # True if the file is private to the user, False if it is public
-    file_path = models.CharField(max_length=100, blank=True) # relative path to the file inside the datastore directory
+    file_path = models.CharField(max_length=100, blank=True)
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     
