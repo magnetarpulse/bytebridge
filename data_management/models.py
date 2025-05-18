803a6190edfa0776eba755070446cccb4edd9c7a
Marija Stojanoska <105957424+marija-stojanoska@users.noreply.github.com>
2025-05-18 18:39:13 -0500

    clickcli upload
    Reuploading models.py that Areena had beforehand because code got messed up not sure how

--- data_management/models.py
+++ data_management/models.py
@@ -6,12 +6,37 @@ from django.db import models
 import uuid
 from django.db import models
 
-class Datastore(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=100)
-    password = models.CharField(max_length=255)
-    instance_id = models.UUIDField()
+
+class Datastores(models.Model):
+    """ Model to store a persistent datastore_id for each user created on an instance of BB """
+
+    owner_id = models.IntegerField(blank=True, null=False) 
+    instance_id = models.UUIDField(default=uuid.uuid4, editable=False) 
+    datastore_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    datastore_name = models.CharField(max_length=100, blank=False)
+    private_permissions = models.BooleanField(default=True) # True if the datastore is private to the user, False if it is public
+    default = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    accessed_at = models.DateTimeField(null=False, blank=False)
 
     def __str__(self):
-        return str(self.id)
+        return f"Datastore for {self.instance_id}: {self.datastore_id}"
 
+
+class Buckets(models.Model):
+    """ Model to store a persistent datastore_id for each user created on an instance of BB """
+
+    owner_id = models.IntegerField(blank=False, null=False) 
+    datastore_id = models.ForeignKey(Datastores, on_delete=models.CASCADE, db_column="datastore_id") # ForeignKey to the UserDatastore model
+    
+    bucket_id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    bucket_name = models.CharField(max_length=100, blank=False)
+    private_permissions = models.BooleanField(default=True) # True if the bucket is private to the user, False if it is public
+    default = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    accessed_at = models.DateTimeField(null=False, blank=False)
+    
+    def __str__(self):
+        return f"Bucket for {self.instance_id}: {self.bucket_id}"
